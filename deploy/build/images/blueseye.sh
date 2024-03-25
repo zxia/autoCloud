76c1ae1dc8aae054050622d703517888ac5e3e6e
xiazw <xiazw@sugon.com>
2024-03-25 16:42:30 +0800

    update the python module path

--- deploy/build/images/blueseye.sh
+++ deploy/build/images/blueseye.sh
@@ -9,18 +9,25 @@ function genBlueseyeDockerFile(){
 FROM python:3.11.0rc2-bullseye
 ENV https_proxy=http://${proxy}
 ENV http_proxy=http://${proxy}
-RUN apt-get update -y
-RUN apt-get install -y expect \
+RUN apt-get update -y &&
+    apt-get install -y  --no-install-recommends\
+     expect \
      rsync \
      jq \
      openssh-server \
      vim \
      dialog \
-     iputils-ping
-RUN apt-get clean
-RUN ln /bin/bash /usr/bin/bash
+     iputils-ping \
+  &&  apt-get clean
+
 COPY ./files/requirements.txt /requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /requirements.txt
+
+# 取消代理设置
+ENV https_proxy=
+ENV http_proxy=
+
+RUN ln -s /bin/bash /usr/bin/bash
 EOF
 
 }
